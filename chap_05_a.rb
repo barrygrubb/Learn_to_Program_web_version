58539304130335a24dbe87ede8a28b8bdd5046cf
barrygrubb <barrygrubb@users.noreply.github.com>
2015-11-26 20:52:13 +0000

    chapter 05 solutions

---
+++ chap_05_a.rb
@@ -0,0 +1,12 @@
+# I guess we could write a program which asks for your first, middle, and last names individually, and then adds those lengths together... hey, why don't you do that!
+
+puts "What is your first name?"
+first_name = gets.chomp
+
+puts "What is your middle name?"
+middle_name = gets.chomp
+
+puts "And what is your last name?"
+last_name = gets.chomp
+
+puts "Well, " + first_name + " " + middle_name + " " + last_name + ", there are " + (first_name.length + middle_name.length + last_name.length).to_s + " characters in your name."
