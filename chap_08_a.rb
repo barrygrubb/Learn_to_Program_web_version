cefd0dc53cb8c3c67e0042fdbdf83d39aac54949
barrygrubb <barrygrubb@users.noreply.github.com>
2015-11-29 10:43:33 +0000

    chapter 08 solutions

---
+++ chap_08_a.rb
@@ -0,0 +1,8 @@
+# Take a quick break and write a program to find out what sayMoo returned.
+
+def sayMoo numberOfMoos
+  puts 'mooooooo...'*numberOfMoos
+end
+
+returnVal =  sayMoo 3
+puts returnVal
